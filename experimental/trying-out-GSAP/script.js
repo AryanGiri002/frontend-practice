a260a1f8786cc1a18522b10a194ab4ba719937e5
Aryan Giri <002giriaryan@gmail.com>
2025-08-10 18:30:55 +0530

    Implement GSAP basics - navigation slide-in, text reveal, and stagger animations

---
+++ experimental/trying-out-GSAP/script.js
@@ -0,0 +1,21 @@
+gsap.from('#nav-left , #nav-right li',{
+    y:-50,
+    opacity:0,
+    duration:1,
+    delay:0.3,
+    stagger:0.2 
+})
+
+gsap.from('#firstpage-bottom p',{
+    y:-5,
+    opacity:0,
+    duration:0.5,
+    delay:1.6,
+})
+
+gsap.from('.gallery-lines h6',{
+    y:122,
+    duration:1,
+    delay:0.5,
+    stagger:0.3
+})
