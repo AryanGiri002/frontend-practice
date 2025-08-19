4aab9db8d0690a9c77b8f39900097db25bd28ba3
Aryan Giri <002giriaryan@gmail.com>
2025-08-19 23:32:39 +0530

    create the parallax menu effect

---
+++ hyperplexed/04-parallax-menu-effect/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#menu');
+
+Array.from(document.querySelectorAll('.menu-item')).forEach((item,index) => {
+    item.onmouseover = () =>{
+        menu.dataset.activeIndex = index;
+    }
+});
